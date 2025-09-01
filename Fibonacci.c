9d3eb3677133e27995321796c15fd4d796354b4c
hinavitha-png <hinavitha@gmail.com>
2025-09-01 12:55:06 +0530

    fibonacci.c

---
+++ Fibonacci.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int n, first = 0, second = 1, next;
+    printf("Enter terms: ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        if (i <= 1) next = i;
+        else {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+        printf("%d ", next);
+    }
+    return 0;
+}
